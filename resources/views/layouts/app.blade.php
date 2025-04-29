7c2bfe0ca5774b17ae06a8c1ccb56c8104d67e3c
Kacper <kgrzesik17@gmail.com>
2025-04-29 23:51:35 +0200

    added basic views + tailwind connection

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sprawiedliwi wśród Ukraińców</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    @include('partials.top')
+
+    <div class="container">
+        @yield('main')
+    </div>
+</body>
+</html>
