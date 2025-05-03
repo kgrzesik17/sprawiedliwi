ba98b171fd959f4b2def29f99b7964b9115e0d46
Kacper <kgrzesik17@gmail.com>
2025-05-03 18:51:34 +0200

    added "konkurs" view

--- resources/views/konkurs.blade.php
+++ resources/views/konkurs.blade.php
@@ -1,1 +1,12 @@
-konkurs
+@extends('layouts.main')
+
+@section('content')
+    <h1>Konkurs na statuetkę</h1>
+    <p>Konkurs na statuetkę „Sprawiedliwi wśród ukraińców” został roztrzygnięty.</p>
+
+    <img src="{{ asset('images/figurka-z-napisem.jpg') }}" alt="zdjęcie statuetki">
+
+    <p>Wesprzyj działania naszej fundacji:</p>
+    <p>Adres kontaktowy: <a id="konkurs" href="mailto:kontakt@propublicobono-rp.org">kontakt@propublicobono-rp.org</a></p>
+    <p>Wsparcie (nr konta): 46 9043 1054 2054 0065 2036 0001</p>
+@endsection
