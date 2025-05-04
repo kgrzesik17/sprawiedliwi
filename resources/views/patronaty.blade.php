a791c9724333bcc00188f8eeb11cc36dcf9ad6f6
Kacper <kgrzesik17@gmail.com>
2025-05-04 13:00:06 +0200

    added hyperlinks to partners sides

--- resources/views/patronaty.blade.php
+++ resources/views/patronaty.blade.php
@@ -11,10 +11,10 @@
 
     <div id="patroni">
         <ul>
-            <li><a href="">portal plportal.pl</a> - Tutaj dowiesz się o najważniejszych wydarzeniach, które miały miejsce w Polsce i na świecie. U nas znajdziesz najświeższe informacje z różnych dziedzin – w tym medium społecznościowe i archiwum życia publicznego</li>
-            <li><a href="">gloswschodu.pl</a> - to portal ukazujący wschód taki jaki on jest i Państwa postsowieckie. Przedstawiamy tam politykę, gospodarkę, turystykę, sport, naukę i technologie, historie, szeroko pojęta kulturę, społeczeństwo. Wyrażamy opinie, komentujemy, pokazujemy, przedstawiamy.</li>
-            <li><a href="">portal cozahistoria.pl</a> - nowoczesny portal historyczny. Artykuły popularnonaukowe, publicystyka, ciekawostki. Najnowsze odkrycia i fragmenty książek historycznych. Udowadniamy, że przeszłość jest ciekawa.</li>
-            <li><a href="">„Podkarpacka Historia”</a> - to projekt medialny mający na celu popularyzację przeszłości Polski południowo-wschodniej. W jego skład wchodzą: ogólnopolski dwumiesięcznik „Podkarpacka Historia”, bezpłatne wydanie specjalne „Z dawnego Rzeszowa” oraz portal www.podkarpackahistoria.pl.</li>
+            <li><a href="https://plportal.pl/" target="_blank">portal plportal.pl</a> - Tutaj dowiesz się o najważniejszych wydarzeniach, które miały miejsce w Polsce i na świecie. U nas znajdziesz najświeższe informacje z różnych dziedzin – w tym medium społecznościowe i archiwum życia publicznego</li>
+            <li><a href="https://gloswschodu.org/" target="_blank">gloswschodu.org</a> - to portal ukazujący wschód taki jaki on jest i Państwa postsowieckie. Przedstawiamy tam politykę, gospodarkę, turystykę, sport, naukę i technologie, historie, szeroko pojęta kulturę, społeczeństwo. Wyrażamy opinie, komentujemy, pokazujemy, przedstawiamy.</li>
+            <li><a href="https://cozahistoria.pl/" target="_blank">portal cozahistoria.pl</a> - nowoczesny portal historyczny. Artykuły popularnonaukowe, publicystyka, ciekawostki. Najnowsze odkrycia i fragmenty książek historycznych. Udowadniamy, że przeszłość jest ciekawa.</li>
+            <li><a href="https://www.podkarpackahistoria.pl/" target="_blank">„Podkarpacka Historia”</a> - to projekt medialny mający na celu popularyzację przeszłości Polski południowo-wschodniej. W jego skład wchodzą: ogólnopolski dwumiesięcznik „Podkarpacka Historia”, bezpłatne wydanie specjalne „Z dawnego Rzeszowa” oraz portal www.podkarpackahistoria.pl.</li>
         </ul>
     </div>
 @endsection
